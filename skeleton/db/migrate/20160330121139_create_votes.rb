315c3f9bd0be2f0eb1df904bc8d55f0f21d1772c
john seo <email": null>
2016-03-30 15:16:14 -0400

    fix migrations and models for questions and votes

--- skeleton/db/migrate/20160330121139_create_votes.rb
+++ skeleton/db/migrate/20160330121139_create_votes.rb
@@ -1,7 +1,8 @@
 class CreateVotes < ActiveRecord::Migration
   def change
     create_table :votes do |t|
-      t.references :vote_type, null: false, index: true
+      t.integer :vote_type_id, null: false, index: true
+      t.string :vote_type, null: false, index: true
       t.references :user, null: false, index: true
       t.integer :source_id, null: false
 
