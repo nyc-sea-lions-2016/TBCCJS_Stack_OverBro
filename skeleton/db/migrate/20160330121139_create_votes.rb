b7d74e0a251adf6cf22ae75742bc65dba9e8c7dc
john seo <email": null>
2016-03-30 15:45:22 -0400

    add answer model/migration

--- skeleton/db/migrate/20160330121139_create_votes.rb
+++ skeleton/db/migrate/20160330121139_create_votes.rb
@@ -1,8 +1,8 @@
 class CreateVotes < ActiveRecord::Migration
   def change
     create_table :votes do |t|
-      t.integer :vote_type_id, null: false, index: true
-      t.string :vote_type, null: false, index: true
+      t.integer :voteable_id, null: false, index: true
+      t.string :voteable_type, null: false, index: true
       t.references :user, null: false, index: true
       t.integer :source_id, null: false
 
