04a697fba2feccfe1d2681a2461436ca1637c423
TimBek2 <tim.beck46@gmail.com>
2016-03-30 16:31:51 -0400

    Fix comment migration, remove obsolete validation from comment model, remove copy pasta line from question model

--- skeleton/db/migrate/20160330143328_create_comments.rb
+++ skeleton/db/migrate/20160330143328_create_comments.rb
@@ -2,10 +2,9 @@ class CreateComments < ActiveRecord::Migration
   def change
   	create_table :comments do |t|
   		t.references :user, index: true, null: false
-  		t.integer :commentable_id
-  		t.string :commentable_type
+  		t.integer :commentable_id, null: false, index: true
+  		t.string :commentable_type, null: false
   		t.text :content, null: false
-  		t.integer :source_id, null: false, index: true
 
       t.timestamps null:false
   	end
