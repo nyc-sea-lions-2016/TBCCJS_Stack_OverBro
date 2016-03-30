092f2b4bd6bb3d687574f9c6a2417b31ef0eaa23
TimBek2 <tim.beck46@gmail.com>
2016-03-30 15:25:06 -0400

    Change name of polymorphic attributes, change to t.string and t.integer instead of t.references for polymorphic attributes

---
+++ skeleton/db/migrate/20160330143328_create_comments.rb
@@ -0,0 +1,12 @@
+class CreateComments < ActiveRecord::Migration
+  def change
+  	create_table :comments do |t|
+  		t.references :user, index: true, null: false
+  		t.integer :commentable_id
+  		t.string :commentable_type
+  		t.text :content, null: false
+  		t.integer :source_id, null: false, index: true
+  		t.boolean :best_answer?, default: false
+  	end
+  end
+end
