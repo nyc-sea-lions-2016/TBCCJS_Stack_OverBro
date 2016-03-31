0683ea62618502c9bfb524aa04ebcee95db3292b
Tim Beck <email": null>
2016-03-31 12:20:25 -0400

    Add new question form to layout so that you can ask a question from any page, not just index

--- skeleton/app/controllers/answers.rb
+++ skeleton/app/controllers/answers.rb
@@ -1,10 +1,11 @@
 post '/answers' do
-
   @answer = Answer.new(params[:answer])
+  question = Question.find(@answer.question_id)
   if @answer.save
 
     redirect "/questions/#{answer.question_id}"
   else
+    @answer_errors = "Invalid Answer"
     redirect '/'
   end
 end
