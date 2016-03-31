fc38bca0d95922bab6971463ca3a347fcd7e4372
CarlinCharge <sprintman210@gmail.com>
2016-03-31 10:17:11 -0400

    adds fixed error handling on registration page and email confirmation validation

--- skeleton/app/controllers/user.rb
+++ skeleton/app/controllers/user.rb
@@ -9,12 +9,14 @@ get '/users/new' do
 end
 
 post '/users' do
-@user = User.new(params[:user])
+
+  @user = User.new(params[:user])
   if @user.save
     session[:user_id] = @user.id
     redirect '/'
   else
     @errors = @user.errors.full_messages
+    p @errors
 
     erb :'/users/new'
   end
