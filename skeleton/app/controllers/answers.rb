4096a5aaaed0a496d88a8ec8ca4dc52bef139b77
Tim Beck <email": null>
2016-03-31 12:25:30 -0400

    Remove unnecessary question identifier from answer post route

--- skeleton/app/controllers/answers.rb
+++ skeleton/app/controllers/answers.rb
@@ -1,6 +1,5 @@
 post '/answers' do
   @answer = Answer.new(params[:answer])
-  question = Question.find(@answer.question_id)
   if @answer.save
     redirect "/questions/#{@answer.question_id}"
   else
