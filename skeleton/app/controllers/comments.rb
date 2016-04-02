b346ad154a472a5b8f8818a9e4762377b074e259
Tim Beck <email": null>
2016-04-02 14:28:39 -0400

    Clean up whitespace in application.js so it looks clearer, fix divs declared within loops so they don't get created multiple times, trying to create a line between each comment to separate them

--- skeleton/app/controllers/comments.rb
+++ skeleton/app/controllers/comments.rb
@@ -1,6 +1,11 @@
 post '/comments' do
   comment = Comment.new(params[:comment])
-  if comment.save
-    erb :'/comments/_new_comment', layout: false, locals: {comment: comment}
-  end
+    if request.xhr?
+      if comment.save
+        erb :'/comments/_new_comment', layout: false, locals: {comment: comment}
+      else
+        @q_comment_errors = "Invalid comment!"
+        erb :'/questions/show'
+      end
+    end
 end
