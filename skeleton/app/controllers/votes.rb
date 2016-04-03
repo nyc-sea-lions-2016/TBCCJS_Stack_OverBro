c5fb205c89ee25a0016c4746ac24cda2c5b2da00
Tim Beck <email": null>
2016-04-02 20:30:07 -0400

    add empty routes for votes on all 3 types of posts

---
+++ skeleton/app/controllers/votes.rb
@@ -0,0 +1,24 @@
+get '/comments/:id/upvote' do
+
+end
+
+get '/comments/:id/downvote' do
+
+end
+
+
+get '/questions/:id/upvote' do
+
+end
+
+get '/questions/:id/downvote' do
+
+end
+
+get '/answers/:id/upvote' do
+
+end
+
+get '/answers/:id/downvote' do
+
+end
